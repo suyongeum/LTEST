f2d0009910a0a9244a598fa194bdead47f53f3e9
suyong eum <suyongeum@gmail.com>
2022-02-14 13:12:09 +0900

    debug_play_stop

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,35 @@
-src_url =`https://github.com/suyongeum/LTEST/blob/master/audios/01_Track.mp3?raw=true`;
-    
+function playAudio(url) {
+    new Audio(url).play();
+}
+
+function linkgeneration(which_cd, item_num) {
+    item = `<audio controls>`;
+    item = item + `<source src=`;
+    if (item_num<10)
+        item = item + `https://github.com/suyongeum/LTEST/blob/master/data/${which_cd}/0${item_num}_Track.mp3?raw=true`;
+    else
+        item = item + `https://github.com/suyongeum/LTEST/blob/master/data/${which_cd}/${item_num}_Track.mp3?raw=true`;
+    item = item + `type="audio/mpeg">`;
+    item = item + `Your browser does not support the audio element.`;
+    item = item + `</audio>`;
+    return item;
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    const ul1 = document.querySelector('.cd1');
+    for (i=1; i<=9; i++) {
+        let node = document.createElement("li");
+        node.className = 'features';
+        node.innerHTML = linkgeneration('FirstCD', i);
+        ul1.appendChild(node);
+    }
+
+    const ul2 = document.querySelector('.cd2');
+    for (i=1; i<=20; i++) {
+        let node = document.createElement("li");
+        node.className = 'features';
+        node.innerHTML = linkgeneration('SecondCD', i);
+        ul2.appendChild(node);
+    }
+});
+
